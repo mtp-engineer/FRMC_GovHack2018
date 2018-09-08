05c3e4d455714da2b5491fc3d6efd87a061bf0b5
Matty <5583895+Mattdoa2@users.noreply.github.com>
2018-09-08 16:32:50 +1000

    Added pretty checkboxes

--- app/aussie-culture-quest/src/scripts/main.js
+++ app/aussie-culture-quest/src/scripts/main.js
@@ -1,10 +1,17 @@
 $(function () {
+    var myNumber = 1;
     $("#generate-list-btn").on("click", function (event) {
-        var features = aashMap.getFeatures();
-        features.forEach(element => {
-            $("#culture-list").append("<li>" + element.Yugambeh_Word + "</li>");
-        });
-        
+        //var features = getFeatures();
+        // features.forEach(element => {
+          
+        $("#culture-list").append(`<label class="container">` + myNumber +
+            `<input type="checkbox" checked="checked">
+            <span class="checkmark"></span>
+            </label>`);
+        // $("#culture-list").append("<li>" + element.Yugambeh_Word + "</li>");
+            //$("#culture-list").append(mymap.features[0].Yugambeh_Word);
+        // });
+        myNumber++;
         // $("#culture-list").append("<li>Added an item</li>");        
     });
 });
