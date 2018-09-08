4a9eb565fa56b20d74bbe80a5ebaef0ce29c5be6
Matty <5583895+Mattdoa2@users.noreply.github.com>
2018-09-08 15:51:25 +1000

    update of js and html

--- app/aussie-culture-quest/src/scripts/main.js
+++ app/aussie-culture-quest/src/scripts/main.js
@@ -1,6 +1,11 @@
 $(function () {
     $("#generate-list-btn").on("click", function (event) {
-        // $.ajax();
-        $("#culture-list").append("<li>Added an item</li>");
+        $.ajax({
+            url: "GoldCoastAnimals.json",
+            success: function(result) {
+                console.log("The result is: " + result);
+                $("#culture-list").append("<li>Added an item</li>");
+            }
+        });        
     });
 });
