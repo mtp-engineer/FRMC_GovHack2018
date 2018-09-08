6b38c42d56715b0264b106c78124f9bf3c5c60a1
Matty <5583895+Mattdoa2@users.noreply.github.com>
2018-09-08 15:57:00 +1000

    script in for sure

--- app/aussie-culture-quest/src/scripts/main.js
+++ app/aussie-culture-quest/src/scripts/main.js
@@ -1,11 +1,10 @@
 $(function () {
     $("#generate-list-btn").on("click", function (event) {
-        $.ajax({
-            url: "GoldCoastAnimals.json",
-            success: function(result) {
-                console.log("The result is: " + result);
-                $("#culture-list").append("<li>Added an item</li>");
-            }
-        });        
+        var features = aashMap.getFeatures();
+        features.forEach(element => {
+            $("#culture-list").append("<li>" + element.Yugambeh_Word + "</li>");
+        });
+        
+        // $("#culture-list").append("<li>Added an item</li>");        
     });
 });
